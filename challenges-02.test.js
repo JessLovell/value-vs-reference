fb74ccdd07b6971ead14912011e9155a240280c4
dhwang21 <hwang.derrick@gmail.com>
2018-09-11 17:55:51 -0700

    uncommented out tests

--- challenges-02.test.js
+++ challenges-02.test.js
@@ -108,44 +108,44 @@ const append = (list1, list2) => {
 //
 // ------------------------------------------------------------------------------------------------
 
-// describe('Testing challenge 1', () => {
-//   test('It should append without modifying the oiginal', () => {
-//     const a = 'This is my story.';
-//     const b = appendTheEnd(a);
+describe('Testing challenge 1', () => {
+  test('It should append without modifying the oiginal', () => {
+    const a = 'This is my story.';
+    const b = appendTheEnd(a);
 
-//     expect(a).toStrictEqual('This is my story.');
-//     expect(b).toStrictEqual('This is my story. The end.');
-//   });
-// });
+    expect(a).toStrictEqual('This is my story.');
+    expect(b).toStrictEqual('This is my story. The end.');
+  });
+});
 
-// describe('Testing challenge 2', () => {
-//   test('It should append by modifying the oiginal', () => {
-//     const a = ['Yes', 'it', 'is'];
-//     appendFirstToLast(a);
+describe('Testing challenge 2', () => {
+  test('It should append by modifying the oiginal', () => {
+    const a = ['Yes', 'it', 'is'];
+    appendFirstToLast(a);
 
-//     expect(a).toStrictEqual(['Yes', 'it', 'is', 'Yes']);
-//   });
-// });
+    expect(a).toStrictEqual(['Yes', 'it', 'is', 'Yes']);
+  });
+});
 
-// describe('Testing challenge 3', () => {
-//   test('It should add a property to an object', () => {
-//     const a = { fullName: 'Octavia Butler' };
-//     addBirthYearProperty(a, 1947);
+describe('Testing challenge 3', () => {
+  test('It should add a property to an object', () => {
+    const a = { fullName: 'Octavia Butler' };
+    addBirthYearProperty(a, 1947);
 
-//     expect(a.yearBorn).toStrictEqual(1947);
-//   });
-// });
+    expect(a.yearBorn).toStrictEqual(1947);
+  });
+});
 
-// describe('Testing challenge 4', () => {
-//   test('It should add a property to every object in an array', () => {
-//     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
-//     setStatusAsAuthor(a);
+describe('Testing challenge 4', () => {
+  test('It should add a property to every object in an array', () => {
+    const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
+    setStatusAsAuthor(a);
 
-//     expect(a[0].isAuthor).toStrictEqual(true);
-//     expect(a[1].isAuthor).toStrictEqual(true);
-//     expect(a[2].isAuthor).toStrictEqual(true);
-//   });
-// });
+    expect(a[0].isAuthor).toStrictEqual(true);
+    expect(a[1].isAuthor).toStrictEqual(true);
+    expect(a[2].isAuthor).toStrictEqual(true);
+  });
+});
 
 describe('Testing challenge 5', () => {
   test('It should append the second array to the first', () => {
